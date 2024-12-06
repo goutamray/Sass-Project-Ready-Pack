2ce5869217d993d8cbfa110cf412dea14eb98da2
goutam <goutamr362@gmail.com>
2024-12-07 01:27:55 +0600

    swiper-js-file-added

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,10 @@ button.addEventListener('click', () => {
 });
 
 
+/**
+ * 
+ * carousel js code 
+ */
 const myCarouselElement = document.querySelector('#carouselExampleIndicators');
  
 const carousel = new bootstrap.Carousel(myCarouselElement, {
@@ -15,6 +19,29 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   touch: true,
   ride: 'carousel',
 });
+
+/**
+ * 
+ * slider js code 
+ */
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
 
 
 
